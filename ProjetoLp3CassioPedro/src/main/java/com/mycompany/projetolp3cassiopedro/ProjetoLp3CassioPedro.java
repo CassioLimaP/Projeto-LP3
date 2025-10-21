08f9ceac570fdf2a08558c9cc39a246ccce8adf9
CassioLimaP <cachedosistema@gmail.com>
2025-10-20 21:22:48 -0300

    criacao do projeto

---
+++ ProjetoLp3CassioPedro/src/main/java/com/mycompany/projetolp3cassiopedro/ProjetoLp3CassioPedro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetolp3cassiopedro;
+
+/**
+ *
+ * @author cassio
+ */
+public class ProjetoLp3CassioPedro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
